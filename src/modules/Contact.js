27a171c0f7667fb5cb5f61647262a2894777bc57
VoidOfMaya <mayaofthevoid@gmail.com>
2025-02-28 17:35:27 +0200

    creates contact info page

--- src/modules/Contact.js
+++ src/modules/Contact.js
@@ -2,9 +2,35 @@
 
 
 const element = document.createElement('div');
-element.innerHTML = "<h1> contact info</h1>";
+element.innerHTML = "<h1 style='justify-Self: center; align-self: center; grid-area: title;'> contact info</h1>";
 element.classList.add('main-title')
 
+const contactCard =document.createElement('div');
+contactCard.style.gridArea = 'contact';
+contactCard.style.backgroundColor = 'rgb(94, 102, 95)';
+contactCard.style.borderRadius = '20px';
+const contacInfo = [ '<li>Phone/Whatsapp: +000-000000000</li>',
+                     '<li>Email : elbaladi@elbaladi.com</li>',
+]
+const contactList = document.createElement('ul');
+contactList.innerHTML = contacInfo.join('');
+contactList.style.color= 'white';
+contactList.style.justifySelf = 'center';
+contactList.style.alignSelf = 'center';
+
+contactCard.appendChild(contactList);
+
+element.style.display = 'grid';
+element.style.height = '92vh';
+element.style.width = '100vw';
+element.style.gridTemplateColumns = '1fr 5fr 1fr'; 
+element.style.gridTemplateRows = '1fr 3fr 1fr';
+element.style.gridTemplateAreas = `
+  "empty-one title empty-two"
+  "empty-one contact empty-two"
+  "empty-one emptyOne empty-two"
+`;
+element.appendChild(contactCard);
 
 
 export { element,}
